50f3e621bb9f57b8268b6ff0eeb75bd88eaca09c
Gabriel Marques Magalhães <gabriel.magalhaes.dev@gmail.com>
2025-06-22 14:10:54 -0300

    Add CircleShape and Player classes for game object representation
    - Implement CircleShape as a base class for game objects.
    - Create Player class inheriting from CircleShape with movement and rotation functionality.
    - Update constants for player attributes.
    - Enhance main game loop to include player updates and rendering.

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 import pygame
 
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from player import Player
 
 
 def main():
     pygame.init()
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+
+    dt = 0
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         # Verifica eventos
@@ -15,8 +21,15 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
+        player.update(dt)  # Atualiza o jogador
+
         screen.fill((0, 0, 0))  # Preenche a tela com preto
+        player.draw(screen)  # Desenha o jogador
+
         pygame.display.flip()  # Atualiza a tela
+
+        # Isso pausará o loop do jogo até que 1/60 de segundo tenha passado.
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
